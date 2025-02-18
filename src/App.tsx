3caaf6e1d977545043769a0a1ea3cbdd38a2d33d
Mickey Serrapica <mickey.serrapica@os.uk>
2025-02-18 12:24:14 +0000

    feat: created component and functions for labels

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { useState } from "react";
+import { LabelButton } from "./components/LabelButton";
 
 function App() {
     const [notes, setNotes] = useState<string[]>([]);
-    const [inputValue, setInputValue] = useState("");
+    const [inputValue, setInputValue] = useState<string>("");
+    const [noteLabels, setNoteLabels] = useState<string[]>([]);
 
     function resetInput(e: React.ChangeEvent<HTMLInputElement>) {
         setInputValue(e.target.value);
@@ -20,9 +22,23 @@ function App() {
     function removeNote(e: React.MouseEvent<HTMLButtonElement>) {
         const idx = e.currentTarget.parentElement?.id;
         if (idx) {
-            setNotes((prev) => prev.filter((_, i) => i !== +Number(idx)));
+            setNotes(prev => prev.filter((_, i) => i !== +(idx)));
+            setNoteLabels(prev => {
+                const newLabels = [...prev];
+                newLabels.splice(+(idx), 1);
+                return newLabels;
+            })
         }
     }
+
+    function addLabel(noteIndex: number, label: string) {
+        setNoteLabels((prev) => {
+            const newLabels = [...prev];
+            newLabels[noteIndex] = label;
+            return newLabels;
+        }
+        );
+      }
 
     return (
         <>
@@ -37,6 +53,8 @@ function App() {
                 {notes.map((note, idx) => (
                     <section id={`${idx}`} className="list-item" key={idx}>
                         <li key={idx}>{note}</li>
+                        {noteLabels[idx] && <span className={`label label-${noteLabels[idx].toLowerCase()}`}>{noteLabels[idx]}</span>}
+                        <LabelButton noteIndex={idx} onLabel={addLabel} />
                         <button type="button" onClick={removeNote}>X</button>
                     </section>
                 ))}
---
+++ src/components/LabelButton.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+interface LabelButton {
+  noteIndex: number
+  onLabel: (noteIndex: number, label: string) => void
+}
+
+export function LabelButton({ noteIndex, onLabel }: LabelButton) {
+  const [isLabeling, setIsLabeling] = useState(false)
+  const labels = ['Important', 'Todo', 'Later']
+
+  return isLabeling ? (
+    <div>
+      {labels.map((label) => (
+        <button
+          key={noteIndex}
+          onClick={() => {
+            onLabel(noteIndex, label)
+            setIsLabeling(false)
+          }}
+        >
+          {label}
+        </button>
+      ))}
+    </div>
+  ) : (
+    <button onClick={() => setIsLabeling(true)}>Add Label</button>
+  )
+}
